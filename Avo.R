87b7b3cc93266fc5e3d28b6ba50ee5af291e2f3c
anthonyjp87 <john.a.perez@gmail.com>
2016-04-28 08:04:13 +0100

    Logistic Regression Iterator

--- Avo.R
+++ Avo.R
@@ -1,15 +1,15 @@
 #Enter Data into Datatable
-foo <- read.csv("foo.csv")
+# foo <- read.csv("foo.csv")
 r <- data.table(foo)
 
 #Clear the empty shit: 
-r[,c('accountid','campaignid','bidderid','clientid','exchangeid','brandid','insertionorderid','inventoryid','tacticflightid','insertionorderflightid','currency','expansions','totalcost','advertiserrevenue','dealid'):=NULL]
+r[,c('accountid','campaignid','bidderid','clientid','externaltagid',' exchangeid','brandid','insertionorderid','inventoryid','tacticflightid','insertionorderflightid','currency','expansions','totalcost','advertiserrevenue','dealid'):=NULL]
 
 #Rename the items in this long stupid thing to easier names:
-unique_item<-unique(r$creativeid)
-  for(i in 1:length(unique_item)){
-    r[creativeid==unique_item[i],creativeid:=as.character(i)]
-  }
+# unique_item<-unique(r$creativeid)
+#   for(i in 1:length(unique_item)){
+#     r[creativeid==unique_item[i],creativeid:=as.character(i)]
+# }
 
 
 print("is na")
@@ -22,7 +22,7 @@ print(sapply(r, function(x) length(unique(x))))
 #1: To clense Data: 
 #binomial regressions: log link function 
 
-day_night <- function(test_hr,b1,b2){
+two_break <- function(test_hr,b1,b2){
 
   test_hr<-ifelse(test_hr==1,1.1,test_hr)
   for(i in 0:6){
@@ -44,8 +44,37 @@ multi_divide <- function(test_hr,b1,b2,b3){
   return(test_hr)
 }
 
-
-
-d_n <- day_night(r$hourofweek,7,18)
-
+d_n <- two_break(r$hourofweek,7,18)
+t_14_17 <- two_break(r$hourofweek,14,17)
 m_d <- multi_divide(r$hourofweek,6,13,20)
+
+
+#2 Set up Logistic Regression of input variables: 
+fit.how <- glm(r$postviewconversions~r$hourofweek, family=binomial)
+display(fit.how)
+
+fit.1 <- glm(r$postviewconversions~d_n, family=binomial)
+display(fit.1)
+# plot(jitter(r$postviewconversions, factor=.5), jitter(d_n, factor=.5), pch=20)
+# curve (invlogit(fit.1$coef[1] + fit.1$coef[2]*x), add=TRUE)
+fit.14_17 <- glm(r$postviewconversions~t_14_17, family=binomial)
+display(fit.14_17)
+
+fit.2 <- glm(r$postviewconversions~d_n + m_d, family=binomial)
+display(fit.2)
+
+fit.3 <- glm(r$postviewconversions~d_n + m_d + d_n:m_d, family=binomial)
+display(fit.3)
+
+#Build iterator that increases the hour each time, builds the vector, regresses the vector on postviewconversions, and tracks coefficients
+fit.it.coef<-rep(0,24)
+fit.it.coef1<-rep(0,24)
+for(n in 1:24){
+  spec_hour <- two_break(r$hourofweek,n-1,n+1)
+  fit.it <- glm(r$postviewconversions~spec_hour, family=binomial)
+  fit.it.coef[n]<-fit.it$coef[1]
+  fit.it.coef1[n]<-fit.it$coef[2]
+}
+plot(fit.it.coef1,xlab = "Hour of Day", ylab="log reg coef")
+
+
